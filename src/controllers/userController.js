f2d498b90cc656cd7055e959c2c808806d27c040
Tanweer Zaya <104303869+zayatanweer@users.noreply.github.com>
2022-10-11 12:02:56 +0530

    new implement

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,22 +18,22 @@ const {
 const createUser = async function (req, res) {
     try {
         let body = req.body;
-        if (!checkEmptyBody(body)) return res(400).send({status: false, message: "please provide data in request body"});
+        if (!checkEmptyBody(body)) return res.status(400).send({status: false, message: "please provide data in request body"});
 
         let { fname, lname, email, profileImage, phone, password, address, ...rest } = body;
         // if (rest)return res(400).send({status: false, message: "please provide required details only"})
 
-        if(!isEmpty(fname)) return res(400).send({status: false, message: "fname is required"});
+        if(!isEmpty(fname)) return res.status(400).send({status: false, message: "fname is required"});
         
-        if(!isValidName(fname)) return res(400).send({status: false, message: "fname is invalid"});
+        if(!isValidName(fname)) return res.status(400).send({status: false, message: "fname is invalid"});
 
-        if(!isEmpty(lname)) return res(400).send({status: false, message: "lname is required"})
+        if(!isEmpty(lname)) return res.status(400).send({status: false, message: "lname is required"})
 
-        if(!isValidName(lname)) return res(400).send({status: false, message: "lname is invalid"})
+        if(!isValidName(lname)) return res.status(400).send({status: false, message: "lname is invalid"})
         
-        if(!isEmpty(email)) return res(400).send({status: false, message: "email is required"})
+        if(!isEmpty(email)) return res.status(400).send({status: false, message: "email is required"})
         
-        if(!isValidEmail(email)) return res(400).send({status: false, message: "lname is invalid"})
+        if(!isValidEmail(email)) return res.status(400).send({status: false, message: "email is invalid"})
         
         // if(!isEmpty(profileImage)) return res(400).send({status: false, message: "profileImage is required"})
         let files = req.files;
@@ -41,41 +41,42 @@ const createUser = async function (req, res) {
         let imageUrl = await uploadFile(files[0])
         body.profileImage = imageUrl;
 
-        if(!isEmpty(phone)) return res(400).send({status: false, message: "phone is required"})
+        if(!isEmpty(phone)) return res.status(400).send({status: false, message: "phone is required"})
 
-        if(!isValidPhone(phone)) return res(400).send({status: false, message: "lname is invalid"})
+        if(!isValidPhone(phone)) return res.status(400).send({status: false, message: "phone is invalid"})
 
-        if(!isEmpty(password)) return res(400).send({status: false, message: "password is required"})
+        if(!isEmpty(password)) return res.status(400).send({status: false, message: "password is required"})
         
-        if(!isValidPassword(password)) return res(400).send({status: false, message: "lname is invalid"})
+        if(!isValidPassword(password)) return res.status(400).send({status: false, message: "password is invalid"})
 
-        if(!isEmpty(address)) return res(400).send({status: false, message: "address is required"})
+        if(!isEmpty(address)) return res.status(400).send({status: false, message: "address is required"})
 
-        if (typeOf(address) != 'object') return res(400).send({status: false, message: "please provide address as object"})
+        // if (typeof(address) != 'object') return res.status(400).send({status: false, message: "please provide address as object"})
 
         const {street, city, pincode} = address.shipping 
-        if(!isEmpty(street)) return res(400).send({status: false, message: "street is required for shipping"})
-        if(!street(street)) return res(400).send({status: false, message: "street is invalid for shipping"})
-        if(!isEmpty(city)) return res(400).send({status: false, message: "city is required for shipping"})
-        if(!city(city)) return res(400).send({status: false, message: "city is invalid for shipping"})
-        if(!isEmpty(pincode)) return res(400).send({status: false, message: "pincode is required for shipping"})
-        if(!pincode(pincode)) return res(400).send({status: false, message: "pincode is invalid for shipping"})
+        if(!isEmpty(street)) return res.status(400).send({status: false, message: "street is required for shipping"})
+        if(!street(street)) return res.status(400).send({status: false, message: "street is invalid for shipping"})
+        if(!isEmpty(city)) return res.status(400).send({status: false, message: "city is required for shipping"})
+        if(!city(city)) return res.status(400).send({status: false, message: "city is invalid for shipping"})
+        if(!isEmpty(pincode)) return res.status(400).send({status: false, message: "pincode is required for shipping"})
+        if(!pincode(pincode)) return res.status(400).send({status: false, message: "pincode is invalid for shipping"})
 
-        if(!isEmpty(address.billing.street)) return res(400).send({status: false, message: "street is required for billing"})
-        if(!street(address.billing.street)) return res(400).send({status: false, message: "street is invalid for billing"})
-        if(!isEmpty(address.billing.city)) return res(400).send({status: false, message: "city is required for billing"})
-        if(!city(address.billing.city)) return res(400).send({status: false, message: "city is invalid for billing"})
-        if(!isEmpty(address.billing.pincode)) return res(400).send({status: false, message: "pincode is required for billing"})
-        if(!pincode(address.billing.pincode)) return res(400).send({status: false, message: "pincode is invalid for billing"})
-
-
+        if(!isEmpty(address.billing.street)) return res.status(400).send({status: false, message: "street is required for billing"})
+        if(!street(address.billing.street)) return res.status(400).send({status: false, message: "street is invalid for billing"})
+        if(!isEmpty(address.billing.city)) return res.status(400).send({status: false, message: "city is required for billing"})
+        if(!city(address.billing.city)) return res.status(400).send({status: false, message: "city is invalid for billing"})
+        if(!isEmpty(address.billing.pincode)) return res.status(400).send({status: false, message: "pincode is required for billing"})
+        if(!pincode(address.billing.pincode)) return res.status(400).send({status: false, message: "pincode is invalid for billing"})
 
 
+        const salt = await bcrypt.genSalt(10);
 
+        let pass= await bcrypt.hash(password, salt);
+        console.log(pass)
 
 
         const createdUser = await userModel.create(body);
-        res.status(201).send({ status: true, message: "User created successfully", data: createdUser });
+        return res.status(201).send({ status: true, message: "User created successfully", data: createdUser });
     }
     catch (error) {
         res.status(500).send({ status: false, error: error.message })
